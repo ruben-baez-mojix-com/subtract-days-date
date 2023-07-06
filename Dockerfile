d2bb016d87595e2418b1c039a0a2c1af359df309
Ruben <ruben.baez@mojix.com>
2023-07-06 09:36:11 -0500

    Give remove chmod && fix ident

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM python:3.7
+FROM python:3-slim
 
 COPY src/ /src/
 
-RUN chmod +x /src/entrypoint.py
-
-ENTRYPOINT ["/src/entrypoint.py"]
+ENTRYPOINT ["python3", "/src/entrypoint.py"]
--- src/entrypoint.py
+++ src/entrypoint.py
@@ -18,26 +18,26 @@ weekdaysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
 
 # Get previous week from last_start date
 def getPreviousByDay(inputDay, start_date=None, daysAgo=7):
-   # check whether the start date is None
-      if start_date is None:
-         # assiging today's date(current date) to the start date
-         start_date = datetime.today().replace(microsecond=0)
-         # getting the weekday number
-         dayNumber = start_date.weekday()
-         # Get the index of the target weekday
-         dayNumberTarget = weekdaysList.index(inputDay)
-         # getting the last weekday
-         daysAgo = (7 + dayNumber - dayNumberTarget) % 7
-      # Subtract the above number of days from the current date(start date)
-      # to get the last week's date of the given day
-      targetDate = start_date - timedelta(days=daysAgo)
-      # returning the last week's date of the input date
-      return targetDate.date()
+    # check whether the start date is None
+    if start_date is None:
+        # assiging today's date(current date) to the start date
+        start_date = datetime.today().replace(microsecond=0)
+        # getting the weekday number
+        dayNumber = start_date.weekday()
+        # Get the index of the target weekday
+        dayNumberTarget = weekdaysList.index(inputDay)
+        # getting the last weekday
+        daysAgo = (7 + dayNumber - dayNumberTarget) % 7
+    # Subtract the above number of days from the current date(start date)
+    # to get the last week's date of the given day
+    targetDate = start_date - timedelta(days=daysAgo)
+    # returning the last week's date of the input date
+    return targetDate.date()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Greetings")
-    parser.add_argument("--date", nargs=1, required=True)
+    parser.add_argument("--date", required=True)
     parser.add_argument("--days", required=False, default=Default.DAYS)
     return parser.parse_args()
 
@@ -49,7 +49,6 @@ def main():
     days = args.days
     last_start_dt = datetime.strptime(date, date_format)
     targetDate = getPreviousByDay(last_start_dt.strftime('%A'), last_start_dt, int(days))
-    
 
     with open(os.environ['GITHUB_OUTPUT'], 'at') as f:
         f.write(f"start_date={str(targetDate)}\n")
